cfca8331d65c0496cc48131fcee07c3daaeaa1af
Ildar Bikmamatov <ildar@bayrell.org>
2021-11-24 10:48:52 +0600

    v1.2.0

--- makefile
+++ makefile
@@ -1,10 +1,12 @@
 
 all:
-	@echo "make {linux|windows}"
+	@echo "make {bdist|compile|clean}"
 
-linux:
-	pyinstaller --onefile -n bayrell_os_desktop_client_1_1 ./run
+compile:
+	pyinstaller --onefile -n bayrell_os_desktop_client_1_2 ./run
 	
+bdist:
+	python3 setup.py sdist bdist_wheel
 
 clean:
 	rm -rf dist/*
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 python3 setup.py sdist bdist_wheel - Сборка пакета
 sudo python3 setup.py develop - Установка пакета для разработки
 sudo python3 setup.py develop -u - Uninstall
-sudo pip3 install dist/bayrell_os_desktop_client-1.1.0.tar.gz - Установка пакета
+sudo pip3 install dist/bayrell_os_desktop_client-1.2.0.tar.gz - Установка пакета
 sudo pip3 uninstall bayrell_os_desktop_client - Удаление пакета
 python3 -m twine upload --repository pypi dist/* - Залить на сервер
 twine upload -r pypi dist/* - Новая команда залить в pypi
@@ -23,7 +23,7 @@ from os.path import abspath, dirname, join
 
 setup(
 	name="bayrell_os_desktop_client",
-	version="1.1.0",
+	version="1.2.0",
 	description="Bayrell OS Desktop Client",
 	long_description=open(join(abspath(dirname(__file__)), 'README.md'), encoding='utf-8').read(),
 	long_description_content_type='text/markdown',
